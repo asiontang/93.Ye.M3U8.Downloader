fdd54e9c5db6ddf39a32a73e6e738b140e08da49
AsionTang <none@none>
2018-06-30 20:35:45 +0800

    +完成了：启动，打开WebUI,杀掉所有进程的功能。

--- HLS.Download.UI/MainForm.Designer.cs
+++ HLS.Download.UI/MainForm.Designer.cs
@@ -31,8 +31,13 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnDoIt = new System.Windows.Forms.Button();
             this.txbUrls = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbLog = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnKillAllAria2 = new System.Windows.Forms.Button();
+            this.btnStartAria2 = new System.Windows.Forms.Button();
+            this.btnOpenAria2WebUI = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -41,9 +46,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.btnDoIt);
             this.groupBox1.Controls.Add(this.txbUrls);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(12, 75);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 108);
+            this.groupBox1.Size = new System.Drawing.Size(448, 79);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "地址";
@@ -54,9 +59,9 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDoIt.Location = new System.Drawing.Point(399, 20);
             this.btnDoIt.Name = "btnDoIt";
-            this.btnDoIt.Size = new System.Drawing.Size(43, 82);
+            this.btnDoIt.Size = new System.Drawing.Size(43, 53);
             this.btnDoIt.TabIndex = 1;
-            this.btnDoIt.Text = "解析";
+            this.btnDoIt.Text = "下载";
             this.btnDoIt.UseVisualStyleBackColor = true;
             this.btnDoIt.Click += new System.EventHandler(this.btnDoIt_Click);
             // 
@@ -68,32 +73,82 @@
             this.txbUrls.Location = new System.Drawing.Point(6, 20);
             this.txbUrls.Multiline = true;
             this.txbUrls.Name = "txbUrls";
-            this.txbUrls.Size = new System.Drawing.Size(386, 82);
+            this.txbUrls.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbUrls.Size = new System.Drawing.Size(386, 53);
             this.txbUrls.TabIndex = 0;
             this.txbUrls.Text = "https://video.fjhps.com/20180303/E1okxWlJ/index.m3u8\r\nhttps://video.fjhps.com/201" +
     "80303/hD8sDBV1/index.m3u8";
+            this.txbUrls.TextChanged += new System.EventHandler(this.txbUrls_TextChanged);
             // 
-            // textBox1
+            // txbLog
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.txbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 126);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(448, 163);
-            this.textBox1.TabIndex = 1;
+            this.txbLog.Location = new System.Drawing.Point(12, 160);
+            this.txbLog.Multiline = true;
+            this.txbLog.Name = "txbLog";
+            this.txbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txbLog.Size = new System.Drawing.Size(448, 129);
+            this.txbLog.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnKillAllAria2);
+            this.groupBox2.Controls.Add(this.btnStartAria2);
+            this.groupBox2.Controls.Add(this.btnOpenAria2WebUI);
+            this.groupBox2.Location = new System.Drawing.Point(12, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(448, 66);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Aria2控制台:";
+            // 
+            // btnKillAllAria2
+            // 
+            this.btnKillAllAria2.Location = new System.Drawing.Point(346, 20);
+            this.btnKillAllAria2.Name = "btnKillAllAria2";
+            this.btnKillAllAria2.Size = new System.Drawing.Size(96, 36);
+            this.btnKillAllAria2.TabIndex = 2;
+            this.btnKillAllAria2.Text = "杀掉所有进程";
+            this.btnKillAllAria2.UseVisualStyleBackColor = true;
+            this.btnKillAllAria2.Click += new System.EventHandler(this.btnKillAllAria2_Click);
+            // 
+            // btnStartAria2
+            // 
+            this.btnStartAria2.Location = new System.Drawing.Point(6, 20);
+            this.btnStartAria2.Name = "btnStartAria2";
+            this.btnStartAria2.Size = new System.Drawing.Size(96, 36);
+            this.btnStartAria2.TabIndex = 2;
+            this.btnStartAria2.Text = "启动";
+            this.btnStartAria2.UseVisualStyleBackColor = true;
+            this.btnStartAria2.Click += new System.EventHandler(this.btnStartAria2_Click);
+            // 
+            // btnOpenAria2WebUI
+            // 
+            this.btnOpenAria2WebUI.Location = new System.Drawing.Point(179, 20);
+            this.btnOpenAria2WebUI.Name = "btnOpenAria2WebUI";
+            this.btnOpenAria2WebUI.Size = new System.Drawing.Size(96, 36);
+            this.btnOpenAria2WebUI.TabIndex = 1;
+            this.btnOpenAria2WebUI.Text = "打开WebUI";
+            this.btnOpenAria2WebUI.UseVisualStyleBackColor = true;
+            this.btnOpenAria2WebUI.Click += new System.EventHandler(this.btnOpenAria2WebUI_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 301);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txbLog);
             this.Controls.Add(this.groupBox1);
             this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,7 +159,11 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnDoIt;
         private System.Windows.Forms.TextBox txbUrls;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txbLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOpenAria2WebUI;
+        private System.Windows.Forms.Button btnStartAria2;
+        private System.Windows.Forms.Button btnKillAllAria2;
     }
 }
 
--- HLS.Download.UI/MainForm.cs
+++ HLS.Download.UI/MainForm.cs
@@ -14,30 +14,137 @@ namespace HLS.Download.UI
             InitializeComponent();
         }
 
+        private string[] getDownloadUrls()
+        {
+            return txbUrls.Text.Split(Environment.NewLine.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private void txbUrls_TextChanged(object sender, EventArgs e)
+        {
+            btnDoIt.Text = string.Format("下载\n({0})", getDownloadUrls().Length);
+        }
+
         private void btnDoIt_Click(object sender, EventArgs e)
         {
-            var settings = new Aria2cSettings();
-            settings.Aria2Path = Path.Combine(Environment.CurrentDirectory, "Aria2\\aria2c.exe");
-            settings.Aria2Host = "localhost";
-            settings.Aria2Port = 6800;
-            Aria2cRuntime.Load(settings);
             try
             {
-                var urls = txbUrls.Text.Split(Environment.NewLine.ToArray());
+                var urls = getDownloadUrls();
                 foreach (var url in urls)
                 {
-                    Debug.WriteLine("url:");
+                    Debug.WriteLine("url：");
                     Debug.WriteLine(url);
                     var task = HLS.Download.Models.HLSStream.Open(url);
-                    Debug.WriteLine("Result:");
+                    Debug.WriteLine("Result：");
                     Debug.WriteLine(task.Result);
 
                 }
             }
             finally
             {
-                Aria2cRuntime.ShutDown();
             }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var TAG = "主程序";
+            WriteLog(TAG, "启动中");
+
+            btnStartAria2.Enabled = !Aria2cRuntime.IsLoaded;
+            WriteLog(TAG, "检测到 Aria2 状态为" + (!btnStartAria2.Enabled ? "【已启动】" : "[未启动]"));
+
+            WriteLog(TAG, "启动完毕。");
+        }
+
+        private void btnStartAria2_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Enabled = false;
+            Cursor.Current = Cursors.WaitCursor;
+            var TAG = "启动Aria2";
+            WriteLog(TAG, "执行中");
+            try
+            {
+                var settings = new Aria2cSettings();
+                settings.Aria2Path = Path.Combine(Environment.CurrentDirectory, "Aria2\\aria2c.exe");
+                settings.Aria2Host = "localhost";
+                settings.Aria2Port = 6800;
+                Aria2cRuntime.Load(settings);
+
+                WriteLog(TAG, "执行完毕。");
+
+                WriteLog(TAG, "执行结果：检测中");
+                btnStartAria2.Enabled = !Aria2cRuntime.IsLoaded;
+                WriteLog(TAG, "执行结果：" + (!btnStartAria2.Enabled ? "【成功】" : "[失败]"));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                WriteLog(TAG, "出现未知异常");
+                WriteLog(TAG, ex.ToString());
+
+                //异常情况下，还是将按钮可用化，方便下次执行。
+                ((Button)sender).Enabled = true;
+            }
+            finally
+            {
+                //由执行结果决定最终是否可用。
+                //((Button)sender).Enabled = true;
+
+                Cursor.Current = Cursors.Default;
+            }
+        }
+
+        private void btnKillAllAria2_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Enabled = false;
+            Cursor.Current = Cursors.WaitCursor;
+            var TAG = "杀掉所有Aria2进程";
+            WriteLog(TAG, "执行中");
+            try
+            {
+                Aria2cRuntime.ShutDown();
+
+                //进程遍历所有相同名字进程。
+                var plist = Process.GetProcessesByName("aria2c");
+                foreach (var p in plist)
+                    if (!p.HasExited)
+                        p.Kill();
+
+                WriteLog(TAG, string.Format("执行完毕:尝试杀掉{0}个进程。", plist.Length + (btnStartAria2.Enabled ? 0 : 1)));
+            }
+            catch (Exception ex)
+            {
+                WriteLog(TAG, "出现未知异常");
+                WriteLog(TAG, ex.ToString());
+            }
+            finally
+            {
+                WriteLog(TAG, "检测是否有进程残留中");
+                btnStartAria2.Enabled = Process.GetProcessesByName("aria2c").Length == 0;
+                WriteLog(TAG, "检测结果：" + (btnStartAria2.Enabled ? "【无残留】" : "[有残留]"));
+
+                Cursor.Current = Cursors.Default;
+                ((Button)sender).Enabled = true;
+            }
+        }
+
+        private void btnOpenAria2WebUI_Click(object sender, EventArgs e)
+        {
+            Process.Start("http://aria2c.com/");
+        }
+
+        private void WriteLog(String tag, String info)
+        {
+#if DEBUG
+            Debug.Write(tag);
+            Debug.Write("：");
+            Debug.Write(info);
+            Debug.WriteLine(info);
+#endif
+
+            txbLog.AppendText(tag);
+            txbLog.AppendText("：");
+            txbLog.AppendText(info);
+            txbLog.AppendText(Environment.NewLine);
         }
     }
 }
