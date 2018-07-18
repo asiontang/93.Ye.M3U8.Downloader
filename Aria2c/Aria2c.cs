997352e9d891a91036affa92a99d994787ac4a7d
AsionTang <none@none>
2018-07-18 17:54:49 +0800

    +完善了:结束下载时,不会因为内部更新计时器已经废弃掉导致的报Null异常.

--- Aria2c/Aria2c.cs
+++ Aria2c/Aria2c.cs
@@ -210,7 +210,8 @@ namespace FlyVR.Aria2
                 //AutoReset设置为False，以便超时后触发一次事件就停止。必须再调用一次Start才能继续下一次触发。
                 //这样可以保证，在Elapsed事件里出现耗时操作时，能够线性执行。否则interval秒内都没执行完成时，
                 //不会出现并发执行耗时动作。
-                timer.Start();
+                if (timer != null)
+                    timer.Start();
             }
         }
 
